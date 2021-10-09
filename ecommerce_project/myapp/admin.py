de9ae99d0dbce606f211e8ee2635796943151b56
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-10-10 01:31:32 +0600

    created models order, orderline, cart, carline

--- ecommerce_project/myapp/admin.py
+++ ecommerce_project/myapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from ecommerce_project.myapp.models import BuyerUser, Address, Company, Category, Product, Cart, Review, Order
+from ecommerce_project.myapp.models import BuyerUser, Address, Company, Category, Product, Cart, Review, Order, \
+    CartLine, OrderLine
 
 admin.site.register(BuyerUser)
 admin.site.register(Company)
@@ -9,3 +10,5 @@ admin.site.register(Product)
 admin.site.register(Cart)
 admin.site.register(Review)
 admin.site.register(Order)
+admin.site.register(CartLine)
+admin.site.register(OrderLine)
--- ecommerce_project/myapp/models.py
+++ ecommerce_project/myapp/models.py
@@ -87,19 +87,37 @@ class Review(models.Model):
 
 class Cart(models.Model):
     unique_id = models.CharField(max_length=50, null=True)
-    products = models.ManyToManyField(Product, related_name='carts_where_this_product', blank=True)
+    # products = models.ManyToManyField(Product, related_name='carts_where_this_product', blank=True)
+    # cart_lines = models.ForeignKey(CartLine, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(BuyerUser,on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         data = self.unique_id
         return data
 
 
+class CartLine(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return "Cart item of " + self.product.name
+
+
 class Order(models.Model):
     buyer = models.ForeignKey(BuyerUser, on_delete=models.CASCADE, related_name='orders_of')
-    products = models.ManyToManyField(Product, blank=True)
-    date = models.DateTimeField(auto_now_add=True, blank=True)
+    # products = models.ManyToManyField(Product, blank=True)
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     value = models.FloatField()
 
     def __str__(self):
         data = "Order of buyer "+ self.buyer.full_name
         return data
+
+
+class OrderLine(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return "Order item of the product" + self.product.name
