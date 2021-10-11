7197c0a033f6aa84b9deb46cf5e7cb9c09dbf5bb
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-10-12 04:03:13 +0600

    category and address, detail, delete, edit done. Address check implemented to check if there is any object associated with it

--- ecommerce_project/myapp/serializers/other_serializers.py
+++ ecommerce_project/myapp/serializers/other_serializers.py
@@ -12,3 +12,79 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+class AddressUpdateSerializer(serializers.Serializer):
+    street_address = serializers.CharField(required=False,max_length=100)
+    city = serializers.CharField(max_length=20,required=False)
+    province = serializers.CharField(max_length=20, required=False)
+    zipcode = serializers.CharField(max_length=10, required=False)
+    pk = serializers.SerializerMethodField()
+    """
+    A serializer can either implement create or update methods or both, as per django rest docs. 
+    """
+    def update(self, instance, validated_data):
+        if 'street_address' in validated_data:
+            instance.street_address = validated_data.get('street_address', instance.street_address)
+        if 'city' in validated_data:
+            instance.city = validated_data.get('city', instance.city)
+        if 'province' in validated_data:
+            instance.province = validated_data.get('province', instance.province)
+        if 'zipcode' in validated_data:
+            instance.zipcode = validated_data.get('zipcode', instance.zipcode)
+
+        instance.save()
+        return instance
+
+    def validate(self, data):
+        """
+        This method can be used later to add any validation, the example here is to demonstrate one validation
+        Check that the remind me date is before the before due date.
+        """
+        # print(data['remind_me_datetime'])
+        # if 'remind_me_datetime' in data:
+        #     if 'due_datetime' in data:
+        #         if not (data['due_datetime'] > data['remind_me_datetime']):
+        #             raise serializers.ValidationError({"remind_me_date": "Reminder date has to be before due date"})
+        #     else:
+        #         instance = getattr(self, 'instance', None)
+        #         if not (instance.due_datetime > data['remind_me_datetime']):
+        #             raise serializers.ValidationError({"remind_me_date": "Reminder date has to be before due date"})
+        return data
+
+    def get_pk(self,obj):
+        return obj.id
+
+
+class CategoryUpdateSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=20, required=False)
+    pk = serializers.SerializerMethodField()
+    """
+    A serializer can either implement create or update methods or both, as per django rest docs. 
+    """
+
+    def update(self, instance, validated_data):
+        if 'name' in validated_data:
+            instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
+
+    def validate(self, data):
+        """
+        This method can be used later to add any validation, the example here is to demonstrate one validation
+        Check that the remind me date is before the before due date.
+        """
+        # print(data['remind_me_datetime'])
+        # if 'remind_me_datetime' in data:
+        #     if 'due_datetime' in data:
+        #         if not (data['due_datetime'] > data['remind_me_datetime']):
+        #             raise serializers.ValidationError({"remind_me_date": "Reminder date has to be before due date"})
+        #     else:
+        #         instance = getattr(self, 'instance', None)
+        #         if not (instance.due_datetime > data['remind_me_datetime']):
+        #             raise serializers.ValidationError({"remind_me_date": "Reminder date has to be before due date"})
+        return data
+
+    def get_pk(self, obj):
+        return obj.id
+
--- ecommerce_project/myapp/views/other_views.py
+++ ecommerce_project/myapp/views/other_views.py
@@ -1,8 +1,11 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from ecommerce_project.myapp.models import Address, Category
-from ecommerce_project.myapp.serializers.other_serializers import AddressSerializer, CategorySerializer
+from ecommerce_project.myapp.serializers.other_serializers import AddressSerializer, CategorySerializer, \
+    AddressUpdateSerializer, CategoryUpdateSerializer
+from django.db import models
 
 
 class AddressListNCreateAPIView(APIView):
@@ -41,3 +44,65 @@ class CategoryListNCreateAPIView(APIView):
             output_serializer = CategorySerializer(created_category)
             return Response(output_serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AddressDetailUpdateDeleteAPIView(APIView):
+    """
+    Retrieve, update or delete a object instance.
+    """
+    # permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk, format=None):
+        address = get_object_or_404(Address, pk=pk)
+        serializer = AddressSerializer(address)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        address = get_object_or_404(Address, pk=pk)
+        # validate_if_post_or_comment_owner_logged_in(request, post)
+        serializer = AddressUpdateSerializer(address, data=request.data)
+        if serializer.is_valid():
+            updated_address = serializer.save()
+            output_serializer = AddressSerializer(updated_address)
+            return Response(output_serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        address = get_object_or_404(Address, pk=pk)
+        #find if this is associated with other models
+        if hasattr(address, 'address_of') or hasattr(address, 'company_address_of'):
+            #means there is a buyer user with this address cannot be deleted
+            # print(address.company_address_of)
+            return Response({"addressError: This address cannot be deleted"}, status=status.HTTP_400_BAD_REQUEST)
+        # print(address.address_of)
+        # validate_if_post_or_comment_owner_logged_in(request, post)
+        address.delete()
+        return Response({"delete": "delete success"},status=status.HTTP_204_NO_CONTENT)
+
+
+class CategoryDetailUpdateDeleteAPIView(APIView):
+    """
+    Retrieve, update or delete a object instance.
+    """
+    # permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk, format=None):
+        category = get_object_or_404(Category, pk=pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        category = get_object_or_404(Category, pk=pk)
+        # validate_if_post_or_comment_owner_logged_in(request, post)
+        serializer = CategoryUpdateSerializer(category, data=request.data)
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            output_serializer = CategorySerializer(updated_category)
+            return Response(output_serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        category = get_object_or_404(Category, pk=pk)
+        # validate_if_post_or_comment_owner_logged_in(request, post)
+        category.delete()
+        return Response({"delete": "delete success"},status=status.HTTP_204_NO_CONTENT)
