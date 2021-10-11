4652fadb173e2e12995ddc2aba25054be5f1fafe
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-10-12 00:04:52 +0600

    buyer update api works

--- ecommerce_project/myapp/views/buyer_views.py
+++ ecommerce_project/myapp/views/buyer_views.py
@@ -1,10 +1,17 @@
-from rest_framework import status
+from django.http import Http404
+from rest_framework import status, serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from ecommerce_project.myapp.models import BuyerUser
 from ecommerce_project.myapp.serializers import BuyerOutputSerializer
-from ecommerce_project.myapp.serializers.buyer_serializers import BuyerInputSerializer
+from ecommerce_project.myapp.serializers.buyer_serializers import BuyerInputSerializer, BuyerUpdateSerializer
+
+
+def get_buyer_user_object(pk):
+    try:
+        return BuyerUser.objects.get(pk=pk)
+    except BuyerUser.DoesNotExist:
+        raise Http404
 
 
 class BuyersUserAPIView(APIView):
@@ -24,3 +31,31 @@ class BuyersUserAPIView(APIView):
             output_serializer = BuyerOutputSerializer(created_buyer)
             return Response(output_serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BuyerDetailUpdateDeleteAPIView(APIView):
+    """
+    Retrieve, update or delete a object instance.
+    """
+    # permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk, format=None):
+        buyer = get_buyer_user_object(pk)
+        serializer = BuyerOutputSerializer(buyer)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        buyer = get_buyer_user_object(pk)
+        # validate_if_post_or_comment_owner_logged_in(request, post)
+        serializer = BuyerUpdateSerializer(buyer, data=request.data)
+        if serializer.is_valid():
+            updated_buyer = serializer.save()
+            output_serializer = BuyerOutputSerializer(updated_buyer)
+            return Response(output_serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        product = get_buyer_user_object(pk)
+        # validate_if_post_or_comment_owner_logged_in(request, post)
+        product.delete()
+        return Response({"delete": "delete success"},status=status.HTTP_204_NO_CONTENT)
--- ecommerce_project/urls.py
+++ ecommerce_project/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('buyers/', views.BuyersUserAPIView.as_view()),
+    path('buyers/<int:pk>/', views.BuyerDetailUpdateDeleteAPIView.as_view()),
     path('sellers/', views.SellersUserAPIView.as_view()),
     path('companies/', views.CompaniesAPIView.as_view()),
     path('addresses/', views.AddressListNCreateAPIView.as_view()),
