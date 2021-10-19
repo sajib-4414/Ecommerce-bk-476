bc8e91672678f019be947dbc5fdba753c936b51b
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-10-19 20:39:18 +0600

    add a user not found case in the login

--- ecommerce_project/myapp/serializers/authentication_serializers.py
+++ ecommerce_project/myapp/serializers/authentication_serializers.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
@@ -10,6 +11,9 @@ class LoginSerializer(serializers.Serializer):
     def create(self, validated_data):
         user_email = validated_data.pop('email')
         password = validated_data.pop('password')
-        user = User.objects.get(email=user_email,password=password)
+        try:
+            user = User.objects.get(email=user_email,password=password)
+        except User.DoesNotExist:
+            raise Http404
         token, created = Token.objects.get_or_create(user=user)
         return { 'token': token}
