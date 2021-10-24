4c0e71d655d0f9ad6718efb853c64e46dcd7e107
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-10-24 13:27:32 +0600

    added a check if the user is admin or buyer and not returning the user if the condition checked

--- ecommerce_project/myapp/serializers/authentication_serializers.py
+++ ecommerce_project/myapp/serializers/authentication_serializers.py
@@ -7,31 +7,31 @@ from ecommerce_project.myapp.serializers import BuyerOutputSerializer
 
 User = get_user_model()
 
-class LoginSerializer(serializers.Serializer):
-    email = serializers.CharField(required=True,max_length=50)
-    password = serializers.CharField(required=True,max_length=20)
-    user = BuyerOutputSerializer()
 
-    def create(self, validated_data):
-        user_email = validated_data.pop('email')
-        password = validated_data.pop('password')
-        try:
-            user = User.objects.get(email=user_email,password=password)
-        except User.DoesNotExist:
-            raise Http404
-        token, created = Token.objects.get_or_create(user=user)
-        return { 'token': token}
+class RequirableBooleanField(serializers.BooleanField):
+    default_empty_html = serializers.empty
+
 
 class LoginSerializer(serializers.Serializer):
     email = serializers.CharField(required=True,max_length=50)
     password = serializers.CharField(required=True,max_length=20)
     user = BuyerOutputSerializer(read_only=True)
+    is_buyer = RequirableBooleanField(required=True)
 
     def create(self, validated_data):
         user_email = validated_data.pop('email')
         password = validated_data.pop('password')
+        is_requested_user_buyer = validated_data.pop('is_buyer')
         try:
             user = User.objects.get(email=user_email,password=password)
+            if is_requested_user_buyer:
+                #check if we got buyer user
+                if not (user.is_staff and not user.is_admin):
+                    raise Http404
+            else:
+                #check if we got seller user, as a seller user is requested
+                if not (user.is_admin and not user.is_staff):
+                    raise Http404
         except User.DoesNotExist:
             raise Http404
         token, created = Token.objects.get_or_create(user=user)
@@ -42,4 +42,5 @@ class LoginSerializer(serializers.Serializer):
         else:
             newdict.update({"seller": buyer.data})
         return newdict
+
 
