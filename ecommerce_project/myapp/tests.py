101b5cb6b365a9e47c1e568c089b1cb7a8267b6a
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-11-05 01:29:09 +0600

    added test case example

--- ecommerce_project/myapp/tests.py
+++ ecommerce_project/myapp/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+"""
+To run the tests here, run the command python manage.py test
+Make sure all the method name starts with test_
+"""
+
+
+class ProductTestCases(TestCase):
+    def test_get_all_products(self):
+        # request_body = {
+        #     'param1  ': 'param1value',
+        #     'param2': 'param2value'
+        # }
+        client = APIClient()
+        response = client.get(path='/products/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
